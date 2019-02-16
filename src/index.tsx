aaa0a635369f196c773a297633e6d73964c7d436
Javier Pascual <javipascualg@gmail.com>
2019-02-16 10:18:37 +0100

    Pass state and actions to app component

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { connect, Provider } from "react-redux";
+import { addEvent, deleteEvent, updateEvent } from "./actions/events";
+import { store } from "./redux_app";
+import { State, StateProps, DispatchProps } from "./types/app";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const mapStateToProps = (state: State): StateProps => ({
+  events: state.events,
+});
 
+const mapDispatchToProps = (dispatch: any): DispatchProps => ({
+  addEvent: (date: string, descr: string) => {
+   dispatch(addEvent(date, descr));
+  },
+  updateEvent: (date: string, descr: string) => {
+   dispatch(updateEvent(date, descr));
+  },
+  deleteEvent: (date: string) => {
+    dispatch(deleteEvent(date));
+  },
+});
+
+const AppContainer = connect<StateProps, DispatchProps, {}, State>(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+ReactDOM.render(<Provider store={store}><AppContainer/></Provider>, document.getElementById('root'));
