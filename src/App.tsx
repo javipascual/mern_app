18b40236674affbbe08439fea44e4f6b9bde9f94
Javier Pascual <javipascualg@gmail.com>
2019-02-16 10:55:36 +0100

    Render stored events

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import './App.css';
 import { Props } from "./types/app"
+import { Event } from "./types/events"
 import Button from 'antd/lib/button';
-import { Input } from 'antd';
+import { Input, List } from 'antd';
 
 type State = {
   date: string;
@@ -28,14 +29,47 @@ class App extends React.Component<Props, State> {
     this.setState(() => ({ date: "", descr: "" }));
   }
 
+  private renderEvent = (e: Event) => (
+    <List.Item>
+      <List.Item.Meta
+        title={e.date}
+        description={e.descr}
+      />
+    </List.Item>
+  );
+
   public render() {
     return (
-      <div className="App">
-        <Input placeholder="Enter description" onChange={this.onDescrChange} />
-        <Button type="primary" onClick={this.onButtonClick}>Add</Button>
+      <div className="App" style={style.container}>
+        <header><h1>Events</h1></header>
+        <main>
+          <List
+            itemLayout="horizontal"
+            dataSource={this.props.events}
+            renderItem={this.renderEvent}
+          />
+        </main>
+        <footer style={style.footer}>  
+          <Input placeholder="Enter description" value={this.state.descr} onChange={this.onDescrChange} />
+          <Button type="primary" onClick={this.onButtonClick}>Add</Button>
+        </footer>
       </div>
     );
   }
 }
+
+// STYLE
+const style: { [key: string]: React.CSSProperties } = {
+  container: {
+    display: "grid",
+    gridTemplateRows: "auto 1fr auto",
+    gridTemplateColumns: "100%",
+    margin: "4rem"
+  },
+  footer: {
+    display: "grid",
+    gridTemplateColumns: "1fr auto",
+  },
+};
 
 export default App;
