57aa357ae11d69ae8e00c1fd4051825df4c8753c
Javier Pascual <javipascualg@gmail.com>
2019-02-21 11:56:15 +0100

    Create root saga

--- src/redux_app.ts
+++ src/redux_app.ts
@@ -1,8 +1,15 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import events from "./reducers/events";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./sagas/events";
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = combineReducers({
   events,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
